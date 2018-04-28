04e213f3b98fc60d58ae3e465f9ca10a6d4cdf7d
Peter Georgiev <pepsiko@mail.bg>
2018-04-28 21:07:37 +0300

    Basic layout

--- Source/SyncBoard/SyncBoard/MainPage.xaml.cs
+++ Source/SyncBoard/SyncBoard/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,11 @@ namespace SyncBoard
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Touch | CoreInputDeviceTypes.Pen;
         }
     }
 }
